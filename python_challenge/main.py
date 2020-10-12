3f47e0e969f52a8b687263cfac61a6065205e57b
William Pappas <williampappas@Williams-MacBook-Air.local>
2020-10-12 17:21:50 -0400

    Challenge Accepted

--- python_challenge/main.py
+++ python_challenge/main.py
@@ -1,49 +1,53 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 11 13:50:57 2020
-
-@author: williampappas
-"""
 #%%
 import os
 import csv
-path = "/Users/williampappas/Desktop/GitLab/02-Homework/03-Python/02-Case-Assignment/Instructions/PyBank/Resources/budget_data.csv"
+path = "/Users/williampappas/Desktop/Python/python_challenge/Resources/budget_data.csv"
 
 bd = os.path.join(path)
 
-month = []
-value = []
-average = []
-maxi = []
-mini = []
+Total_Months = 0
+Net_Profit = 0
+Change = 0
+Average_Change = 0
+NatChangeList = []
+GreatestInc = ["",0]
+GreatestDec = ["",999999]
 
-with open(bd, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader,None)
+with open(bd, 'r') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    next(csvreader)
+    first_data = next(csvreader)
+    Total_Months = Total_Months + 1  
+    Net_Profit = Net_Profit + int(first_data[1])
+    previous_data = int(first_data[1])
     for row in csvreader:
-        ref = len(list(csvreader))
-        month = str(row[0])
-        integer = [int(row[1]) for row in csvreader]
-        value = sum(integer)
-        average = sum(integer) / ref
-        maxi = max(integer)
-        mini = min(integer)
+        Total_Months = Total_Months + 1
+        Net_Profit = Net_Profit + int(row[1])
+        Change = int(row[1]) - previous_data
+        previous_data = int(row[1])
+        NatChangeList.append(Change)
+        if Change > GreatestInc[1]:
+            GreatestInc[1] = Change
+            GreatestInc[0] = row[0]
+        if Change < GreatestDec[1]:
+            GreatestDec[1] = Change
+            GreatestDec[0] = row[0]
         
-print(f"Financial Analysis")
-print(f"-----------------------------------")
-print(f"Total Months: {ref}")
-print(f"Total: ${value}")
-print(f"Average Change: ${average}")
-print(f"Greatest Increase in Profits: {month} (${maxi})")
-print(f"Greatest Decrease in Profits: {month} (${mini})")
-     
-# %%
-output = "/Users/williampappas/Desktop/Python/python_challenge/Resources/Budget.csv"
-data = zip(month, value, average, maxi, mini)
-output_file = os.path.join(output)
-with open(output_file, "w", newline='') as datafile:
-    writer = csv.writer(datafile)
-    writer.writerow(["Total Months", "Net Profit", "Average Change","Greatest Increase in Profits","Greatest Decrease in Profits"])
-    writer.writerows(data)
-         
+Average_Change = round(sum(NatChangeList) / len(NatChangeList), 2)
+
+output = (
+    "\nFinancial Analysis\n"
+    "---------------------------------\n"
+    f"Total Months: {Total_Months}\n"
+    f"Net Profit: ${Net_Profit}\n"
+    f"Average Change: ${Average_Change}\n"
+    f"Greatest Increase in Profits: {GreatestInc[0]}, ${GreatestInc[1]}\n"
+    f"Greatest Decrease in Profits: {GreatestDec[0]}, ${GreatestDec[1]}")
+print(output)
+
+file = "/Users/williampappas/Desktop/Python/python_challenge/Analysis/Budget.txt"
+filetooutput = os.path.join(file)
+with open(file, "w", newline='') as datafile:
+    datafile.write(output)
+
+
